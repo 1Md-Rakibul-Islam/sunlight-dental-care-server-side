86629d6a72656b7626d59544900b56d31f36a928
1Md-Rakibul-Islam <rakibul199tech@gmail.com>
2022-11-10 15:00:10 +0600

    service based rivew api added

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
-
-
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 require('dotenv').config();
 const app = express();
@@ -14,11 +13,90 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json())
 
+//DB: SunlightDentalCareDB
+// pass: AXFkQhj3JB3FaDry
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.gksews0.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    const dentalServicesCollection = client.db('SunlightDentalCare').collection('services');
+
+    const servicesReviewCollection = client.db('SunlightDentalCare').collection('review');
+
+    try{
+
+        // all services load api
+        app.get('/topservice', async(req, res) => {
+            const query = {};
+            const cursor = dentalServicesCollection.find(query);
+            const services = await cursor.limit(3).toArray()
+            res.send(services);
+        })
+
+        //read data from database and send to client site
+        app.get('/services', async(req, res) => {
+            const query = {};
+            const cursor = dentalServicesCollection.find(query);
+            const services = await cursor.toArray()
+            res.send(services);
+        })
+
+        //read data from client site and insert to database
+        app.post('/services', async(req, res) => {
+            const service = req.body;
+            const result = await dentalServicesCollection.insertOne(service);
+            res.send(result);
+        })
+
+        // spicific id based service load api
+        app.get('/services/:_id', async(req, res) => {
+            const id = req.params._id;
+            const query = { _id: ObjectId(id)};
+            const service = await dentalServicesCollection.findOne(query);
+            res.send(service);
+        })
+
+        //review data read from client site and insert to database
+        app.post('/reviews', async(req, res) => {
+            const review = req.body;
+            console.log(review)
+            const result = await servicesReviewCollection.insertOne(review);
+            res.send(result)
+        })
+
+        // spicific service based review load api
+        // app.get('/reviews/:_', async(req, res) => {
+        //     const id = req.params._id;
+        //     const query = { _id: ObjectId(id)};
+        //     const reviews = await servicesReviewCollection.find(query);
+        //     res.send(reviews);
+        // })
+
+        app.get('/reviews', async(req, res) => {
+            let query = {};
+            if(req.query.service){
+                query = {
+                    service: req.query.service
+                }
+            }
+            const cursor = servicesReviewCollection.find(query);
+            const reviews = await cursor.toArray();
+            res.send(reviews);
+        })
+
+    }
+    finally{
+
+    }
+}
+run().catch(err => console.error(err))
 
 app.get('/', (req, res) => {
-    res.send('Sinlight Dental Care server is running')
+    res.send('Sunlight Dental Care server is running')
 })
 
 app.listen(port, () => {
-    console.log(`Sinlight Dental Care server running on ${port}`);
+    console.log(`Sunlight Dental Care server running on ${port}`);
 })
