db28bcc8f58f6fc33ea8a71e0bbb0b05be1e1379
1Md-Rakibul-Islam <rakibul199tech@gmail.com>
2022-11-11 16:19:29 +0600

    remove user and password for DB

--- index.js
+++ index.js
@@ -13,9 +13,6 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json())
 
-//DB: SunlightDentalCareDB
-// pass: AXFkQhj3JB3FaDry
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.gksews0.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -66,14 +63,6 @@ async function run() {
             res.send(result)
         })
 
-        // //review data read from database and send client side
-        // app.get('/reviews/:_id', async(req, res) => {
-        //     const id = req.params._id;
-        //     const query = { _id: ObjectId(id)};
-        //     const review = await servicesReviewCollection.findOne(query);
-        //     res.send(review);
-
-        // })
 
         //delete review
         app.delete('/reviews/:_id', async(req, res) => {
