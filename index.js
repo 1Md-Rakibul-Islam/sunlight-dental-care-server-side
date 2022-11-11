0351c68d5a11fddec2ef38688a28ba56f1259bde
1Md-Rakibul-Islam <rakibul199tech@gmail.com>
2022-11-11 16:21:25 +0600

    sempale modify

--- index.js
+++ index.js
@@ -17,6 +17,9 @@ app.use(express.json())
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.gksews0.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+
+//server side code
+
 async function run() {
     const dentalServicesCollection = client.db('SunlightDentalCare').collection('services');
 
